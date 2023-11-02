bfad5b9187602fc97facfe542bf6a2741e306da5
Vince Agbayani <JohnVincentAgbayani@Deltek.com>
2023-11-02 11:51:56 +0800

    updated to read from jenkins parameters

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ import os
 ssm_file = open("unlock_ad_ssm.json")
 ssm_json = ssm_file.read()
 
-target_environment = "Deltekdev"
-target_username = "johnvincentagbayani"
+env_file = open("temp_env.txt")
+target_environment = env_file.read()
+target_environment = target_environment.replace("\n","")
+
+user_file = open("temp_user.txt")
+target_username = user_file.read()
+target_username = target_username.replace("\n","")
 
 instance_ids = {
 	"Deltekdev":"i-04d0e953afe07b3a3",
@@ -24,7 +29,7 @@ ssm_client = boto3.client('ssm', region_name="us-east-1")
 
 ssm_create_response = ssm_client.create_document(Content = ssm_json, Name = ssm_doc_name, DocumentType = 'Command', DocumentFormat = 'JSON', TargetType =  "/AWS::EC2::Instance")
 
-ssm_run_response = ssm_client.send_command(InstanceIds = [target_instance_id], DocumentName=ssm_doc_name, DocumentVersion="$DEFAULT", TimeoutSeconds=120)
+ssm_run_response = ssm_client.send_command(InstanceIds = [target_instance_id], DocumentName=ssm_doc_name, DocumentVersion="$DEFAULT", TimeoutSeconds=120,  Parameters={'Username':[target_username]})
 print(f'{ssm_run_response}\n')
 cmd_id = ssm_run_response['Command']['CommandId']
 
